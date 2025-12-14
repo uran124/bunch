2a3fdceee24e729ea31802e38053a214c252fa40
uran124 <55823948+uran124@users.noreply.github.com>
2025-12-14 22:45:58 +0700

    Update Controller.php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -6,8 +6,8 @@ abstract class Controller
     protected function getDadataSettings(): array
     {
         return [
-            'apiKey' => '6e4950476cc01a78b287788434dc1028eb3e86cf',
-            'secretKey' => 'f2b84eb0e15b3c7b93c75ac50a8cd53b1a9defa1',
+            'apiKey' => 'd32d23e2087d406928a38947855d3179f03dcff2',
+            'secretKey' => 'b11fccf3100a8666f0cb5382071f2e935c449df9',
             'suggestions' => true,
             'geocoding' => true,
             'dailyLimit' => 1500,
