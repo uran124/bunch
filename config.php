41e0740db712f6840050cfe17f121ab724bbfa45
uran124 <55823948+uran124@users.noreply.github.com>
2026-04-09 01:35:16 +0700

    Update config.php

--- config.php
+++ config.php
@@ -21,10 +21,10 @@ $defineIfMissing = static function (string $key, mixed $value): void {
 };
 
 // База данных
-$defineIfMissing('DB_HOST', $readEnv('DB_HOST') ?? 'localhost');
+$defineIfMissing('DB_HOST', $readEnv('DB_HOST') ?? '188.127.239.143');
 $defineIfMissing('DB_NAME', $readEnv('DB_NAME') ?? 'bunch');
 $defineIfMissing('DB_USER', $readEnv('DB_USER') ?? 'bunch');
-$defineIfMissing('DB_PASS', $readEnv('DB_PASS') ?? '');
+$defineIfMissing('DB_PASS', $readEnv('DB_PASS') ?? 'tM1pE1iN5g');
 
 // Общие настройки приложения
 $defineIfMissing('APP_ENV', $readEnv('APP_ENV') ?? 'prod'); // prod | dev
@@ -35,8 +35,8 @@ $defineIfMissing('APP_TIMEZONE', $readEnv('APP_TIMEZONE') ?? 'Asia/Krasnoyarsk')
 date_default_timezone_set(APP_TIMEZONE);
 
 // DaData credentials
-$defineIfMissing('DADATA_API_KEY', $readEnv('DADATA_API_KEY') ?? '');
-$defineIfMissing('DADATA_SECRET_KEY', $readEnv('DADATA_SECRET_KEY') ?? '');
+$defineIfMissing('DADATA_API_KEY', $readEnv('DADATA_API_KEY') ?? 'd32d23e2087d406928a38947855d3179f03dcff2');
+$defineIfMissing('DADATA_SECRET_KEY', $readEnv('DADATA_SECRET_KEY') ?? 'b11fccf3100a8666f0cb5382071f2e935c449df9');
 
 // Internal Telegram relay API (VPS -> old hosting)
 $defineIfMissing('TG_INTERNAL_API_KEY_ID', $readEnv('TG_INTERNAL_API_KEY_ID') ?? 'bunch-bot-v1');
